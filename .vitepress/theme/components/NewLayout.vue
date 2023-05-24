1beb1c2027431e1032ccce84712b2e84eb7eba62
Hacker-C <1507559148@qq.com>
2023-05-24 19:31:10 +0800

    🐛 fix: 目录显示异常

--- .vitepress/theme/components/NewLayout.vue
+++ .vitepress/theme/components/NewLayout.vue
@@ -1,12 +1,10 @@
 <template>
   <Layout />
   <Copyright />
-  <!-- <Catalog />  -->
 </template>
 <script lang="ts" setup>
 import DefaultTheme from 'vitepress/theme'
 import Copyright from './Copyright.vue'
-import Catalog from './Catalog.vue'
 
 const { Layout } = DefaultTheme
 </script>
